a66a091fcf926792c47871759ca035a59b73703c
Devansh Pandey <pandeydevanshhhh@gmail.com>
2026-07-24 10:26:31 +0530

    first commit 24/07/26

---
+++ prg2.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+
+using namespace std;
+
+void show(int x,int y)
+{
+    cout<<"x="<<x<<" y="<<y<<endl;
+}
+
+void swapByVal(int x,int y)
+{
+    int z=x;
+    x=y;
+    y=z;
+    
+}
+
+void swapByAddress(int *x,int *y)
+{
+    int z=*x;
+    *x=*y;
+    *y=z;
+}
+
+void swapByRef(int &x,int &y)
+{
+    int z=x;
+    x=y;
+    y=z;
+}
+
+int main()
+{
+
+    int x,y;
+    cout<<"enter value of x and y"<<endl;
+    cin>>x>>y;
+    
+    cout<<"Swapping by value"<<endl;
+    cout<<"Value Before swap :"<<endl;
+    show(x,y);
+    swapByVal(x,y);
+    cout<<"Value After swap :"<<endl;
+    show(x,y);
+
+
+    cout<<"Swap by address"<<endl;
+    cout<<"Value Before swap :"<<endl;
+    show(x,y);
+    swapByAddress(&x,&y);
+    cout<<"Value After swap :"<<endl;
+    show(x,y);
+
+
+    cout<<"Swap by reference"<<endl;
+    cout<<"Value Before swap :"<<endl;
+    show(x,y);
+    swapByRef(x,y);
+    cout<<"Value After swap :"<<endl;
+    show(x,y);
+
+
+    return 0;
+}
